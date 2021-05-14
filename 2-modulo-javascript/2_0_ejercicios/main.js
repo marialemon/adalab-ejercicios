257e259b8ba5283282305531cfb362e6d02b3e92
marialemon <maria.nc@hotmail.es>
2021-05-14 10:30:05 +0200

    add to do list exercise

--- 2-modulo-javascript/2_0_ejercicios/main.js
+++ 2-modulo-javascript/2_0_ejercicios/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 ////////2_01 INTRO A LA PROGRAMACIÓN
-/*
+
 //Ejercicio 2
 
 let address = "Calle Falsa 123";
@@ -87,7 +87,7 @@ formElement.addEventListener("change", handlerForm);
 */
 
 // EJERCICIO 8
-/*
+
 const strawberry = document.querySelector(".fruit-strawberry");
 const banana = document.querySelector(".fruit-banana");
 const kiwi = document.querySelector(".fruit-kiwi");
@@ -124,17 +124,16 @@ ulTeachers.addEventListener("click", addClass);
 ////////2_07 BUCLES
 
 let movies = ["whiplash", "interestelar", "the greatest showman"];
-movies[2].push("dooonie");
+
+movies.push("dooonie");
 
 console.log(movies);
-
-*/
 
 ////////2_08 ARRAYS Y BUCLES 2
 
 //Ejercicio 1
 
-console.log("EJERCICIO 1");
+console.log("2.08 Arrays y bucles: EJERCICIO 1");
 
 const arr = [];
 
@@ -149,15 +148,15 @@ get100Numbers();
 
 //Ejercicio 2
 
-console.log("EJERCICIO 2");
+console.log("2.08 Arrays y bucles: EJERCICIO 2");
 
 const arr1 = [];
 
 function getReversed100Numbers() {
   for (let i = 0; i < 100; i++) {
     arr1.push(i);
-    arr1.reverse();
   }
+  arr1.reverse();
   console.log(arr1);
 }
 
@@ -165,7 +164,7 @@ getReversed100Numbers();
 
 //Ejercicio 3
 
-console.log("EJERCICIO 3: APARTADO 1");
+console.log("2.08 Arrays y bucles: EJERCICIO 3: APARTADO 1");
 
 const lostNumbers = [4, 8, 15, 16, 23, 31, 42];
 
@@ -189,11 +188,12 @@ function bestLostNumber() {
 
 bestLostNumber();
 
-console.log("EJERCICIO 3: APARTADO 2");
+console.log("2.08 Arrays y bucles: EJERCICIO 3: APARTADO 2");
 
 const lostNumbersMultiple = [];
 
 function multipleNumber() {
+  //Crear un segundo array (mul) en el que se incluyen los números que se extraen del primer array
   for (let i = 0, mul = 0; i < lostNumbers.length; i++)
     if (lostNumbers[i] % 3 === 0) {
       lostNumbersMultiple[mul++] = lostNumbers[i];
@@ -202,3 +202,73 @@ function multipleNumber() {
 }
 
 multipleNumber();
+
+console.log("2.08 Arrays y bucles: EJERCICIO 3: APARTADO 3");
+
+let concat = lostNumbersEven.concat(lostNumbersMultiple);
+
+console.log(concat);
+
+//Ejercicio 4
+
+console.log("2.08 Arrays y bucles: EJERCICIO 4");
+
+//Pintar el listado de tareas
+
+const ulToDoList = document.querySelector(".js-todolist");
+const tasks = [
+  {
+    name: "Recoger setas en el campo",
+    completed: true,
+  },
+  {
+    name: "Comprar pilas",
+    completed: true,
+  },
+  {
+    name: "Poner una lavadora de blancos",
+    completed: true,
+  },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+  },
+];
+
+function paintTasks() {
+  let html = "";
+  let className = "";
+  let checked = "";
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    if (tasks[i].completed === true) {
+      className = "crossout";
+      checked = "checked";
+    } else {
+      className = "";
+      checked = "";
+    }
+    html += `<li class= "${className}">`;
+    html += `<input class= "js-checkbox" type="checkbox" value="${i}">`;
+    html += `${task.name}"</li>`;
+  }
+  ulToDoList.innerHTML = html;
+  listenClick();
+}
+
+function listenClick() {
+  const checkboxElements = document.querySelectorAll(".js-checkbox");
+  for (let i = 0; i < checkboxElements.length; i++) {
+    checkboxElements[i].addEventListener("change", handlerCheck);
+  }
+}
+
+function handlerCheck(e) {
+  console.log(e.target.value);
+  const clicked = e.target.value;
+  tasks[clicked].completed = !tasks[clicked].completed;
+  console.log(tasks);
+  paintTasks();
+}
+
+paintTasks();
